b7c88fb3271e82fad99aafc6942c96f75c2146e1
Andrew Backes <backes.andrew@gmail.com>
2016-03-16 19:53:48 -0500

    Incrementing version number

--- mtgsdk/config.py
+++ mtgsdk/config.py
@@ -8,7 +8,7 @@
 # http://www.opensource.org/licenses/MIT-license
 # Copyright (c) 2016, Andrew Backes <backes.andrew@gmail.com>
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 __pypi_packagename__ = "mtgsdk"
 __github_username__ = "MagicTheGathering"
 __github_reponame__ = "mtg-sdk-python"
